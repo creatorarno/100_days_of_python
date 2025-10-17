08bdf1deec4bcf4eed543ddf648f8d803eca37a5
Ahmad Khan <65672249+creatorarno@users.noreply.github.com>
2025-10-18 00:40:26 +0530

    Add brand name generator script
    Introduces a Python script that generates a brand name by combining the user's home city and pet name. Prompts for input and displays the result.

---
+++ day01/brand_name_generator.py
@@ -0,0 +1,11 @@
+# -- WELCOME --
+print("Welcome to the Brand Name Generator.")
+
+# ask for home city name
+city = input("What's the name of the city you grew up in?: ")
+
+# ask for pet name
+pet_name = input("What's your pet's name?: ")
+
+# create brand name
+print(city, pet_name)
